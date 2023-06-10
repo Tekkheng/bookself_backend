ee634e6b8b408b36cb580e1b786b6f94fe5c5f8d
tekkheng <tekkheng22@gmail.com>
2023-06-10 17:33:48 +0700

    update

---
+++ tes.js
@@ -0,0 +1,51 @@
+const databook = [
+    {
+        id: 22,
+        name: "asas",
+        publisher: "adsad",
+        umur: 232
+    },
+    {
+        id: 24,
+        name: "asas",
+        publisher: "adsad",
+        umur: 232
+    }
+]
+const s = databook.forEach((d)=>{
+    return d.id == 22
+})
+console.log(s);
+// console.log(databook.id);
+// console.log(books);
+
+books = [
+    {
+        id: "Qbax5Oy7L8WKf74l",
+        name: "Buku A",
+        year: 2010,
+        author: "John Doe",
+        summary: "Lorem ipsum dolor sit amet",
+        publisher: "Dicoding Indonesia",
+        pageCount: 100,
+        readPage: 25,
+        finished: false,
+        reading: false,
+        insertedAt: "2021-03-04T09:11:44.598Z",
+        updatedAt: "2021-03-04T09:11:44.598Z"
+    },
+    {
+        id: "Qbax5Oy7L8WKf74l",
+        name: "Buku A",
+        year: 2010,
+        author: "John Doe",
+        summary: "Lorem ipsum dolor sit amet",
+        publisher: "Dicoding Indonesia",
+        pageCount: 100,
+        readPage: 25,
+        finished: false,
+        reading: false,
+        insertedAt: "2021-03-04T09:11:44.598Z",
+        updatedAt: "2021-03-04T09:11:44.598Z"
+    },
+]
